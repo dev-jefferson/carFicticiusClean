ebc9de8fdb64650ce0475c11714bbca45392ac56
Jefferson Henrique Sousa Galvao <jefferson.galvao@lifecon.com.br>
2022-05-02 04:06:32 -0300

    iniciando projeto

---
+++ src/main/java/br/com/jhsgdev/carFicticiusClean/CarFicticiusCleanApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhsgdev.carFicticiusClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFicticiusCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarFicticiusCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jhsgdev/carFicticiusClean/CarFicticiusCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhsgdev.carFicticiusClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFicticiusCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
