c988b09765ffb11b7fe131b09a68378ccba8a051
Jefferson Henrique Sousa Galvao <jefferson.galvao@lifecon.com.br>
2022-05-03 22:44:42 -0300

    adicionando ExceptionResponse

---
+++ src/main/java/br/com/jhsgdev/carFicticiusClean/exception/ExceptionResponse.java
@@ -0,0 +1,21 @@
+package br.com.jhsgdev.carFicticiusClean.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Date timestamp;
+	private String message;
+	private String details;
+
+}
